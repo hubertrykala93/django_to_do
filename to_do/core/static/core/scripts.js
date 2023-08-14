cfa1e98533282277f8c26aa625ae55f855b7f8a0
Szymon-Levy <94991990+Szymon-Levy@users.noreply.github.com>
2023-08-14 11:51:48 +0200

    .

---
+++ to_do/core/static/core/scripts.js
@@ -0,0 +1,19 @@
+const loginButtons = document.getElementById('login-buttons')
+const loginButton = document.getElementById('login-button')
+const registerButton = document.getElementById('register-button')
+const loginForm = document.getElementById('login-form')
+const registerForm = document.getElementById('register-form')
+
+loginForm.classList.add('active')
+
+loginButtons.addEventListener('click', (e) => {
+   console.log('kfjrr')
+   console.log(e.target.id)
+   if (e.target.id = 'login-button') {
+       loginForm.classList.add('active')
+       registerForm.classList.remove('active')
+   } else if (e.target.id = 'register-button') {
+       loginForm.classList.remove('active')
+       registerForm.classList.add('active')
+   }
+})
