43d495f96cc029405a9951e2711c1226188c2919
Szymon-Levy <94991990+Szymon-Levy@users.noreply.github.com>
2023-08-20 14:02:10 +0200

    mobile styles

--- to_do/core/static/core/scripts.js
+++ to_do/core/static/core/scripts.js
@@ -1,19 +1,11 @@
-//const loginButtons = document.getElementById('login-buttons')
-//const loginButton = document.getElementById('login-button')
-//const registerButton = document.getElementById('register-button')
-//const loginForm = document.getElementById('login-form')
-//const registerForm = document.getElementById('register-form')
-//
-//loginForm.classList.add('active')
-//
-//loginButtons.addEventListener('click', (e) => {
-//   console.log('kfjrr')
-//   console.log(e.target.id)
-//   if (e.target.id = 'login-button') {
-//       loginForm.classList.add('active')
-//       registerForm.classList.remove('active')
-//   } else if (e.target.id = 'register-button') {
-//       loginForm.classList.remove('active')
-//       registerForm.classList.add('active')
-//   }
-//})
+const nav = document.querySelector('#nav')
+const offcanvasToggler = document.querySelector('#offcanvas-toggler')
+const offcanvasClose = document.querySelector('#offcanvas-close')
+
+offcanvasToggler.addEventListener('click', () => {
+    nav.classList.add('active')
+})
+
+offcanvasClose.addEventListener('click', () => {
+    nav.classList.remove('active')
+})
