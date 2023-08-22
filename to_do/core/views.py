40258d717a727fc9767de60851c5163a4596260f
Hubert Rykała <hubert.rykala@gmail.com>
2023-08-22 09:29:15 +0200

    gitignore_update_3

--- to_do/core/views.py
+++ to_do/core/views.py
@@ -3,6 +3,10 @@ from .forms import ContactForm
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def index(request):
@@ -18,18 +22,21 @@ def support(request):
                              message='Your email message has been sent successfully. '
                                      'We will respond as soon as possible.')
 
+            html_message = render_to_string(template_name='core/email_content.html', context={
+                'full_name': contact_form.cleaned_data['full_name'],
+                'email': contact_form.cleaned_data['email'],
+                'mobile_phone': contact_form.cleaned_data['mobile_phone'],
+                'message': contact_form.cleaned_data['message'],
+                'file': contact_form.cleaned_data['file']
+            }, request=request)
+
             send_mail(
                 subject=f"Message from {contact_form.cleaned_data['full_name']}.",
                 message=contact_form.cleaned_data['message'],
                 from_email=contact_form.cleaned_data['email'],
-                recipient_list=['hubert.rykala@gmail.com', 'szymon.levy123@gmail.com'],
-                html_message=render_to_string(template_name='core/email_content.html', context={
-                    'full_name': contact_form.cleaned_data['full_name'],
-                    'email': contact_form.cleaned_data['email'],
-                    'mobile_phone': contact_form.cleaned_data['mobile_phone'],
-                    'message': contact_form.cleaned_data['message'],
-                    'file': contact_form.cleaned_data['file']
-                }, request=request)
+                recipient_list=[os.environ.get('EMAIL_LOGIN')],
+                fail_silently=True,
+                html_message=html_message
             )
 
             return redirect(to='index')
