a2a3505300e7dc3fc7bb7497ed3b5521574e6c60
Hubert Rykała <hubert.rykala@gmail.com>
2023-08-22 08:11:53 +0200

    environ variables added

--- to_do/to_do/settings.py
+++ to_do/to_do/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,9 +32,9 @@ ALLOWED_HOSTS = []
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_FROM = 'hubert.rykala@gmail.com'
-EMAIL_HOST_USER = 'hubert.rykala@gmail.com'
-EMAIL_HOST_PASSWORD = 'bsvdctbnvaqlszhd'
+EMAIL_FROM = os.environ.get('EMAIL_LOGIN')
+EMAIL_HOST_USER = os.environ.get('EMAIL_LOGIN')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
