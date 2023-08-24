ee260a7d0c301bcde833dfd4cd1d05b184dc411a
Szymon-Levy <94991990+Szymon-Levy@users.noreply.github.com>
2023-08-24 21:18:26 +0200

    styling app body and header

--- to_do/core/static/core/scripts.js
+++ to_do/core/static/core/scripts.js
@@ -9,3 +9,19 @@ offcanvasToggler.addEventListener('click', () => {
 offcanvasClose.addEventListener('click', () => {
     nav.classList.remove('active')
 })
+
+
+//theme change
+const themeToggler = document.querySelector('#theme-changer')
+
+themeToggler.addEventListener('click', () => {
+    if ( document.documentElement.getAttribute('theme') === 'dark' ){
+        document.documentElement.removeAttribute('theme')
+        themeToggler.querySelector('i').classList.remove('ri-sun-line')
+        themeToggler.querySelector('i').classList.add('ri-moon-fill')
+    }else {
+        document.documentElement.setAttribute('theme', 'dark')
+        themeToggler.querySelector('i').classList.remove('ri-moon-fill')
+        themeToggler.querySelector('i').classList.add('ri-sun-line')
+    }
+})
