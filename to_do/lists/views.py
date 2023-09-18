2a36bed25a058132a11700bc4220d9a42dda555a
Hubert Rykała <hubert.rykala@gmail.com>
2023-09-18 21:25:37 +0200

    pagination deleted

--- to_do/lists/views.py
+++ to_do/lists/views.py
@@ -7,17 +7,10 @@ from django.core.paginator import Paginator
 @login_required
 def lists(request):
     categories = Category.objects.filter(user=request.user)
-    tasks_list = Task.objects.all()
-
-    paginator = Paginator(object_list=tasks_list, per_page=8)
-    page = request.GET.get('page')
-    tasks = paginator.get_page(number=page)
-
-    tasks.number
+    tasks = Task.objects.all()
 
     return render(request=request, template_name='lists/lists.html', context={
         'title': 'Lists',
         'categories': categories,
-        'tasks_list': tasks_list,
         'tasks': tasks
     })
