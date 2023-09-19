ac7bd49a023819a8172779f01a2628fc44ae0a91
Hubert Rykała <hubert.rykala@gmail.com>
2023-09-19 17:12:14 +0200

    tasks update

--- to_do/lists/views.py
+++ to_do/lists/views.py
@@ -9,12 +9,8 @@ def lists(request):
     categories = Category.objects.filter(user=request.user)
     tasks = Task.objects.all()
 
-    paginator = Paginator(object_list=tasks, per_page=8)
-    page = paginator.page(number=2)
-
     return render(request=request, template_name='lists/lists.html', context={
         'title': 'Lists',
         'categories': categories,
         'tasks': tasks,
-        'page': page
     })
