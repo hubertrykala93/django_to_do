17c2ca9e2d9f69d151e215e146d4edf78cc8bef8
Hubert Rykała <hubert.rykala@gmail.com>
2023-09-24 22:58:32 +0200

    delete category complete

--- to_do/lists/views.py
+++ to_do/lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Task
 from django.contrib.auth.decorators import login_required
 from .forms import CategoryForm
@@ -13,15 +13,21 @@ def lists(request):
     if request.method == 'POST':
         category_form = CategoryForm(data=request.POST)
 
-        if category_form.is_valid():
-            category = category_form.save(commit=False)
-            category.user = request.user
-            category.save()
+        if not Category.objects.filter(category=request.POST.get('category', None)).exists():
+            if category_form.is_valid():
+                category = category_form.save(commit=False)
+                category.user = request.user
+                category.save()
 
-            messages.success(request=request, message='Your category has been added successfully.')
+                messages.success(request=request, message='Your category has been added successfully.')
+
+                redirect(to='lists')
+
+            else:
+                messages.error(request=request, message='Your category has not been added successfully.')
 
         else:
-            messages.error(request=request, message='Your category has not been added successfully')
+            messages.info(request=request, message=f"Category {request.POST.get('category', None)} already exists.")
 
     else:
         category_form = CategoryForm()
@@ -33,24 +39,13 @@ def lists(request):
         'category_form': category_form
     })
 
-# @login_required
-# def lists(request):
-#     categories = Category.objects.filter(user=request.user)
-#     tasks = Task.objects.all()
-#
-#     category = request.POST.get('category', None)
-#     print(category)
-#
-#     if Category.objects.filter(category=category).exists():
-#         messages.info(request=request, message='This category already exists.')
-#     else:
-#         Category.objects.create(user=request.user, category=category)
-#
-#         messages.success(request=request,
-#                          message=f"Category {request.POST.get('category').title()} has been added successfully.")
-#
-#     return render(request=request, template_name='lists/lists.html', context={
-#         'title': 'Lists',
-#         'categories': categories,
-#         'tasks': tasks
-#     })
+
+def add_category(request):
+    pass
+
+
+def delete_category(request, pk):
+    category = Category.objects.get(pk=pk)
+    category.delete()
+
+    return redirect(to='lists')
