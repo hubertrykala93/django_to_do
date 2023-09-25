15bfa7e268a9feaafa711f574ab6488aa739327f
Hubert Rykała <hubert.rykala@gmail.com>
2023-09-25 17:21:30 +0200

    update

--- to_do/lists/urls.py
+++ to_do/lists/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path(route='lists/', view=views.lists, name='lists'),
-    path(route='update-category/', view=views.update_category, name='update-category'),
     path(route='delete-category/<int:pk>', view=views.delete_category, name='delete-category')
 ]
