bf40ee1d4e051b36cf52b515392ad34396b49f8a
Hubert Rykała <hubert.rykala@gmail.com>
2023-10-22 17:52:59 +0200

    allowed hosts edited

--- to_do/to_do/settings.py
+++ to_do/to_do/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
-
+ALLOWED_HOSTS = ['195.242.116.113', '127.0.0.1', 'localhost']
 
 # Application definition
 
