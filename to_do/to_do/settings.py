d640587405be79aef74e1c0020d097bf2ed8c493
Hubert Rykała <hubert.rykala@gmail.com>
2023-10-22 13:37:32 +0200

    settings deploy update

--- to_do/to_do/settings.py
+++ to_do/to_do/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0olo&8f%eh5e=*rhmm7x8*&4rlz*&czpq9+95q+p*h5uo_#&)z"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['195.242.116.113']
 
 # Application definition
 
